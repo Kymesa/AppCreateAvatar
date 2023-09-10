968e950c2163d00bdbfdb065fb7479d22ccd22b3
Kymesa <Kymesa@unicesar.edu.co>
2023-09-10 18:03:01 -0500

    5.0.0

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,6 @@
+import { SplashScreen } from "expo-router";
 import { Stack } from "expo-router/stack";
-
+SplashScreen.preventAutoHideAsync();
 export default function Layout() {
   return <Stack />;
 }
